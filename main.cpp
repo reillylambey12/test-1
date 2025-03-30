ae019785622cbf7f325c768e0ee0efe03ebd8cfb
Reilly Lambey <2022156895@ub.edu.bz>
2025-03-30 17:36:59 -0600

    initial commit

--- main.cpp
+++ main.cpp
@@ -4,22 +4,22 @@
 int main() {
     BinaryTree tree;
     // Insert values
-    tree.insert(1000);
-    tree.insert(354);
-    tree.insert(70);
+    tree.insert(100);
+    tree.insert(324);
+    tree.insert(710);
     tree.insert(25);
-    tree.insert(600);
+    tree.insert(680);
     tree.insert(50);
     tree.insert(820);
 // Inorder traversal
     std::cout << "Inorder traversal: ";
     tree.inorder(); // Output: 20 30 40 50 60 70 80
     // Search for values
-    std::cout << "Searching for 820: " << (tree.search(820) ? "Found" : "Not Found") << std::endl; // Output: Found
+    std::cout << "Searching for 25: " << (tree.search(25) ? "Found" : "Not Found") << std::endl; // Output: Found
     std::cout << "Searching for 900: " << (tree.search(900) ? "Found" : "Not Found") << std::endl; // Output: Not Found
     // Delete a value
-    tree.deleteNode(354);
-    std::cout << "Inorder traversal after deleting 354: ";
+    tree.deleteNode(100);
+    std::cout << "Inorder traversal after deleting 100: ";
     tree.inorder(); 
     return 0;
 }
